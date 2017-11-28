c2a5fb6f7029c0116f5fb4e6f138e98ce57c19c0
amanueladdisu <34046996+amanueladdisu@users.noreply.github.com>
2017-11-27 21:17:55 -0500

    Update hello.java

--- src/basics/hello.java
+++ src/basics/hello.java
@@ -1,10 +1,11 @@
+
 package basics;
 
 public class hello {
 	public static void main(String[] args){
 		System.out.println("hello world!");
 		System.out.println("  i am adding new repository");
-		
+		System.out.println("i love you abuye");
 		int temprature ;
 
 		
